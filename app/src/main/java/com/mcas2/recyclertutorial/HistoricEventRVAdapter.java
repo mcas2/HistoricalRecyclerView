71243f0901ef143421745830bc75a47771c8164d
mcas2 <mcas2@tutanota.com>
2024-01-15 20:01:19 +0100

    Funcionando

---
+++ app/src/main/java/com/mcas2/recyclertutorial/HistoricEventRVAdapter.java
@@ -0,0 +1,65 @@
+package com.mcas2.recyclertutorial;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+/*
+* Creamos una clase Adapter que extiende de RecyclerView.Adapter,
+* cuyo tipo está definida por una * clase interna que definiremos abajo
+* y que extiende de RecyclerView.ViewHolder
+*
+* Creamos un constructor al que le llegue el contexto y el array
+* de clases instanciadas del modelo.
+*
+* Construimos de abajo hacia arriba.
+* */
+
+public class HistoricEventRVAdapter extends RecyclerView.Adapter<HistoricEventRVAdapter.MyViewHolder> {
+    Context context;
+    ArrayList<HistoricEventModel> historicEventModels;
+
+    public HistoricEventRVAdapter(Context context, ArrayList<HistoricEventModel> historicEventModels) {
+        this.context = context;
+        this.historicEventModels = historicEventModels;
+    }
+
+    @NonNull
+    @Override
+    public HistoricEventRVAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.rv_row, parent, false);
+        return new HistoricEventRVAdapter.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HistoricEventRVAdapter.MyViewHolder holder, int position) {
+        //El holder, que es del tipo subclase adaptador que hemos creado, contiene sus vistas, así que podemos llamarlas
+        holder.tvName.setText(historicEventModels.get(position).getEventName());
+        holder.tvDate.setText(historicEventModels.get(position).getEventDate());
+        holder.tvLocation.setText(historicEventModels.get(position).getEventLocation());
+    }
+
+    @Override
+    public int getItemCount() {
+        return historicEventModels.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView tvName, tvDate, tvLocation;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvName = itemView.findViewById(R.id.eventName);
+            tvDate = itemView.findViewById(R.id.eventDate);
+            tvLocation = itemView.findViewById(R.id.eventLocation);
+
+        }
+    }
+}
